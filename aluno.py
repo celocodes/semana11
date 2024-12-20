a3b55d761b70aa0d1b426b4300a260289445d5de
cel01_ <102827591+celocodes@users.noreply.github.com>
2024-12-19 21:15:55 -0300

    Update aluno.py

--- aluno.py
+++ aluno.py
@@ -8,6 +8,11 @@ class AlunoClass:
         return 'Aluno: ' + self.nome + ' ' + self.sobrenome + ' - Nota: ' + str(self.nota)
 
     def salvar(self, conexao, colecao):
+        # Transforma o objeto Aluno em um dicionário
         mydict = self.__dict__
+        
+        # Insere o dicionário na coleção MongoDB
         x = conexao[colecao].insert_one(mydict)
+        
+        # Retorna True se o salvamento foi bem-sucedido
         return x.acknowledged
